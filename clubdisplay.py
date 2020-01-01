ec0dde1dc2855abeafcee4e0b89c9f53e7c796e2
thefentoncreeper <stephenearljenkins@hotmail.com>
2020-01-01 00:58:10 -0500

    RGBled added

--- clubdisplay.py
+++ clubdisplay.py
@@ -4,6 +4,7 @@
 # updates
 # 2019-12-31 init
 
+
 #
 # imports
 #
@@ -12,6 +13,7 @@ import time
 import argparse
 import logging
 import logging.handlers
+from gpiozero import LED, RGBLED
 
 #
 # parsing
@@ -21,7 +23,17 @@ parser.add_argument("-t",
                     "--test",
                     action="store_true",
                     help="for offline testing")
+parser.add_argument(
+    "-d",
+    "--delay",
+    default=0,
+    type=int,
+    help="delay time in mills",
+)
 args = parser.parse_args()
+
+delay = int(args.delay)
+
 
 #
 # logging
@@ -50,9 +62,25 @@ fh.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(fh)
 
+
+#
+# variables
+#
+if not args.test:
+    signal = LED(4)
+    rgb = RGBLED(red=17, green=27, blue=22)
+
+
 #
 # defined functions
 #
+def startstoppulse():
+    if not args.test:
+        signal.on()
+        time.sleep(0.5)
+        signal.off()
+    return
+
 
 #
 # first run items
@@ -65,11 +93,48 @@ logger.addHandler(fh)
 def main():
     timestamp = dt.datetime.now().time()
     logger.info("nowtime = " + str(timestamp)[:5])
+    startstoppulse()
+    stage = 0
+    r = 0
+    g = 0
+    b = 0
 
     while True:
         try:
-            print("testing")
-            time.sleep(60) # wait one minute
+            if stage == 0:
+                # blue to violet
+                r = r + 1
+                if r == 255:
+                    stage = stage + 1
+            elif stage == 1:
+                # violet to red
+                g = g + 1
+                if g == 255:
+                    stage = stage + 1
+            elif stage == 2:
+                # red to yellow
+                b = b + 1
+                if b == 255:
+                    stage = stage + 1
+            elif stage == 3:
+                # yellow to green
+                g = g - 1
+                if g == 0:
+                    stage = stage + 1
+            elif stage == 4:
+                # green to teal
+                r = r - 1
+                if r == 0:
+                    stage = stage +1
+            elif stage == 5:
+                # teal to blue
+                b = b - 1
+                if b == 0:
+                    stage = 0
+
+            rgb.color = (r/255, g/255, b/255)
+            print("r ="+str(r)[:3]+", g="+str(g)[:3]+", b="+str(b)[:3])
+            time.sleep(delay / 1000) # wait delay-time
 
             timestamp = dt.datetime.now().time()
             if args.test:
@@ -78,11 +143,14 @@ def main():
         except KeyboardInterrupt:
             print("\n\nKeyboard exception.  Exiting.\n")
             logger.info("keyboard exception")
+            startstoppulse()
             exit()
 
         except Exception:
             logger.info("program end: " + str(sys.exc_info()[0]))
+            startstoppulse()
             exit()
+    startstoppulse()
     return
 
 if __name__ == "__main__":
